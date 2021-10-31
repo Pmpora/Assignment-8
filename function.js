1452ad62c21d66b9df2186b593aa67e224e275fc
Pmpora <89966591+Pmpora@users.noreply.github.com>
2021-10-31 16:09:00 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,31 @@
+let myName = "Patience";
+let age = 21
+
+function greetMe() {
+    console.log(myName, age);
+}
+
+function  greetMeOthers(myName, age) {
+    console.log(myName, age);   
+}
+
+greetMeOthers("Patienece", 21)
+greetMeOthers("Pamella", 22)
+greetMeOthers("Paul", 24)
+greetMeOthers("Alison", 23)
+greetMeOthers("Joanita", 42)
+// functions that print my name and age, and that of four others
+
+
+let add= (x, y) => x+y;
+console.log (add(2,8));
+//arrow function having two local variables concatenated together
+
+
+
+let numberlimits= 20;
+for (let num = 1; num < numberlimits; num++) {
+    const element = numberlimit[num];
+    console.log("I love Uganda",num)
+}
+//Loop that prints "I love Uganda" 19 times
